793c56948d4615c0ee91b24ee95c0d6dd4ff2fe3
Senad Shabaj <senadshabaj73@gmail.com>
2022-11-18 14:48:41 -0800

    Slash Commands

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request , Response
 from slackeventsapi import SlackEventAdapter
 
 
@@ -17,7 +17,7 @@ slack_event_adapter = SlackEventAdapter(
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")["user_id"]
 
-
+message_counts = {}
 
 @slack_event_adapter.on('message')
 def message(payload):
@@ -26,9 +26,23 @@ def message(payload):
     channel_id = event.get('channel')
     user_id = event.get('user')
     text = event.get('text')
+    
     if BOT_ID != user_id:
-        client.chat_postMessage(channel=channel_id, text=text)
+        if user_id in message_counts:
+            message_counts[user_id] += 1
+        else:
+            message_counts[user_id] = 1
+            
+        
 
 
+@app.route('/message-count', methods=['POST'])
+def message_count():
+    data = request.form
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    message_count = message_counts.get(user_id, 0)
+    client.chat_postMessage(channel=channel_id, text=f'Message: {message_count}')
+    return Response(), 200
 if __name__ == "__main__":
     app.run(debug=True)
